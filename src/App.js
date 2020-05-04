fa8738c75391f4216d750d6f31a6a2837e32c3e7
marobayos <marobayos@unal.edu.co>
2020-05-03 22:38:55 -0500

    :sparkles: Version 0 of chats feature (without back connection)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Chats from './components/Chats.js'
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext } from 'react'
+import { withRouter } from 'react-router-dom'
+
+const Login = props => {
+
+    const [loginCredentials, setLoginCredentials] = useState({ email: "", password: "" });
+
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default withRouter(Login)
